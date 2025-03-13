ebe524220dc2b1d69d68baf79883a6f5a71d0572
Saqib Diar <saqib.diar@bcp.com.pk>
2025-03-14 01:35:08 +0500

    title component is created and added inside latest collection

--- ecommerce/src/main.jsx
+++ ecommerce/src/main.jsx
@@ -3,9 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import ShopContextProvider from './context/ShopContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
+  {/* now we will gonna have support of context api in our project */}
+  <ShopContextProvider> 
     <App />
+  </ShopContextProvider>
   </BrowserRouter>,
 )
