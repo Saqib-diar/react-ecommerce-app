52c4776c1726334a9c6ad31f51242217ae1c9422
Saqib Diar <saqib.diar@bcp.com.pk>
2025-03-14 01:09:35 +0500

    minor changes

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -18,15 +18,15 @@ function App() {
     <div className='px-4 sm:px-[5vw] md:px-[7vw] md:px-[9vw]'>
       <Navbar />
       <Routes>
-        <Route path='/' element={Home}/>
-        <Route path='/collection' element={Collection} />
-        <Route path='/about' element={About} />
-        <Route path='/contact' element={Contact} />
-        <Route path='/product/:productId' element={Product} />
-        <Route path='/cart' element={Cart} />
-        <Route path='/login' element={Login}/>
-        <Route path='/place-order' element={PlaceOrder}/>
-        <Route path='/order' element={Orders}/>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/collection' element={<Collection/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/product/:productId' element={<Product/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/place-order' element={<PlaceOrder/>}/>
+        <Route path='/order' element={<Orders/>}/>
       </Routes>
     </div>
   )
